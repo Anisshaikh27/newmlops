8e8bf9f9a6cf65d5b35b51dd44135aace159e9b6
Anisshaikh27 <anisshaikh2708@gmail.com>
2025-05-13 19:51:08 +0530

    added finals to github

---
+++ src/mlProject/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+CONFIG_FILE_PATH = Path(r"C:\Users\SHREE\OneDrive\Desktop\newmlops\config\config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
+
+print(CONFIG_FILE_PATH)
